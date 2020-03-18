a2cd01161761d153d1b633ccf8a7750ce770407a
James Doyle <james2doyle@gmail.com>
2020-03-17 21:15:09 -0700

    Added: support for pwa plugin

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,16 @@
 // https://cli.vuejs.org/config/#configuration-reference
 const package = require('./package.json');
 
+process.env.VUE_APP_VERSION = package.version;
+
 const repoName = package.repository.url.match(/\/\w+\/(.*?)\.git$/i)[1];
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
+  pwa: {
+    themeColor: '#edf2f7',
+    msTileColor: '#1a202c',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  }
 };
