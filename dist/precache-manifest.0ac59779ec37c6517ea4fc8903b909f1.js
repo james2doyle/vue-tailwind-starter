a9f4193a0fa525180400ec67faa66c6f850e025b
James Doyle <james2doyle@gmail.com>
2020-05-12 14:55:24 -0700

    Changed: build output

--- dist/precache-manifest.0ac59779ec37c6517ea4fc8903b909f1.js
+++ dist/precache-manifest.0ac59779ec37c6517ea4fc8903b909f1.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "31203ec9fe5e6748e31b",
-    "url": "/vue-tailwind-starter/css/app.0831eb2d.css"
+    "revision": "c00cf5362938ffb5bc42",
+    "url": "/vue-tailwind-starter/css/app.88f248f0.css"
   },
   {
     "revision": "6b03b6d2d963afd943f7952ad172114c",
     "url": "/vue-tailwind-starter/img/logo.6b03b6d2.png"
   },
   {
-    "revision": "28247552417f28ae4e6c678fddc81213",
+    "revision": "ff8472e2be4d52809c7c41540eb74b7a",
     "url": "/vue-tailwind-starter/index.html"
   },
   {
-    "revision": "31203ec9fe5e6748e31b",
-    "url": "/vue-tailwind-starter/js/app.399afaf5.js"
+    "revision": "c00cf5362938ffb5bc42",
+    "url": "/vue-tailwind-starter/js/app.11e82183.js"
   },
   {
-    "revision": "15eaf8dc87ec48e747bd",
-    "url": "/vue-tailwind-starter/js/chunk-vendors.cd299e77.js"
+    "revision": "9647ab53b9ada7f2e20b",
+    "url": "/vue-tailwind-starter/js/chunk-vendors.d26fa416.js"
   },
   {
     "revision": "cffc9809281e1a1fda56edf74c041a3b",
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vue-tailwind-starter/precache-manifest.eacd6e7f191043ea1cd4246a8d250b1d.js"
+  "/vue-tailwind-starter/precache-manifest.0ac59779ec37c6517ea4fc8903b909f1.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-tailwind-starter"});
